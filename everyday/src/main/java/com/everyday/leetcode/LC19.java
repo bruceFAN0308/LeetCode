c2237b8f898ba36c85ba5a4fb377705d35bdd5ad
Bruce <bruce.fan0308@gmail.com>
2024-03-04 15:46:33 -0500

    [UPDATE] daily

--- everyday/src/main/java/com/everyday/leetcode/LC19.java
+++ everyday/src/main/java/com/everyday/leetcode/LC19.java
@@ -41,42 +41,6 @@ public class LC19 {
         current.next = current.next.next;
         return head;
 
-//
-//        stack.add(listNode.val);
-//
-//        ListNode node = null;
-//        int num = 0;
-//        while (!stack.isEmpty()) {
-//            num++;
-//            if (num == n) {
-//                stack.pop();
-//                continue;
-//            }
-//            node = new ListNode(stack.pop(), node);
-//        }
-//        return node;
     }
-
-//    public ListNode removeNthFromEnd(ListNode head, int n) {
-//        ListNode dummy = new ListNode(0, head);
-//        int length = getLength(head);
-//        ListNode cur = dummy;
-//        for (int i = 1; i < length - n + 1; ++i) {
-//            cur = cur.next;
-//        }
-//        cur.next = cur.next.next;
-//        ListNode ans = dummy.next;
-//        return ans;
-//    }
-
-    public int getLength(ListNode head) {
-        int length = 0;
-        while (head != null) {
-            ++length;
-            head = head.next;
-        }
-        return length;
-    }
-
 
 }
